b6d1943f5ccbded34054f5a5f7a2d39eb5798560
Jian HanBin <jhanbin@lscm.ad>
2016-10-26 14:46:35 +0800

    20130315

---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.prefix = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.suffix = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.TextBox();
+            this.companyname = new System.Windows.Forms.TextBox();
+            this.companywebsite = new System.Windows.Forms.TextBox();
+            this.addrcompany1 = new System.Windows.Forms.TextBox();
+            this.addrcompany2 = new System.Windows.Forms.TextBox();
+            this.pncompany = new System.Windows.Forms.TextBox();
+            this.pnmobile1 = new System.Windows.Forms.TextBox();
+            this.pnmobile2 = new System.Windows.Forms.TextBox();
+            this.pndirectline = new System.Windows.Forms.TextBox();
+            this.pnfax = new System.Windows.Forms.TextBox();
+            this.email1 = new System.Windows.Forms.TextBox();
+            this.email2 = new System.Windows.Forms.TextBox();
+            this.imskype = new System.Windows.Forms.TextBox();
+            this.immsn = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // prefix
+            // 
+            this.prefix.Location = new System.Drawing.Point(12, 41);
+            this.prefix.Name = "prefix";
+            this.prefix.Size = new System.Drawing.Size(519, 22);
+            this.prefix.TabIndex = 1;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(12, 69);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(519, 22);
+            this.name.TabIndex = 1;
+            // 
+            // suffix
+            // 
+            this.suffix.Location = new System.Drawing.Point(12, 97);
+            this.suffix.Name = "suffix";
+            this.suffix.Size = new System.Drawing.Size(519, 22);
+            this.suffix.TabIndex = 1;
+            // 
+            // title
+            // 
+            this.title.Location = new System.Drawing.Point(12, 125);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(519, 22);
+            this.title.TabIndex = 1;
+            // 
+            // companyname
+            // 
+            this.companyname.Location = new System.Drawing.Point(12, 153);
+            this.companyname.Name = "companyname";
+            this.companyname.Size = new System.Drawing.Size(519, 22);
+            this.companyname.TabIndex = 1;
+            // 
+            // companywebsite
+            // 
+            this.companywebsite.Location = new System.Drawing.Point(12, 181);
+            this.companywebsite.Name = "companywebsite";
+            this.companywebsite.Size = new System.Drawing.Size(519, 22);
+            this.companywebsite.TabIndex = 1;
+            // 
+            // addrcompany1
+            // 
+            this.addrcompany1.Location = new System.Drawing.Point(12, 209);
+            this.addrcompany1.Name = "addrcompany1";
+            this.addrcompany1.Size = new System.Drawing.Size(519, 22);
+            this.addrcompany1.TabIndex = 1;
+            // 
+            // addrcompany2
+            // 
+            this.addrcompany2.Location = new System.Drawing.Point(12, 237);
+            this.addrcompany2.Name = "addrcompany2";
+            this.addrcompany2.Size = new System.Drawing.Size(519, 22);
+            this.addrcompany2.TabIndex = 1;
+            // 
+            // pncompany
+            // 
+            this.pncompany.Location = new System.Drawing.Point(12, 265);
+            this.pncompany.Name = "pncompany";
+            this.pncompany.Size = new System.Drawing.Size(519, 22);
+            this.pncompany.TabIndex = 1;
+            // 
+            // pnmobile1
+            // 
+            this.pnmobile1.Location = new System.Drawing.Point(12, 293);
+            this.pnmobile1.Name = "pnmobile1";
+            this.pnmobile1.Size = new System.Drawing.Size(519, 22);
+            this.pnmobile1.TabIndex = 1;
+            // 
+            // pnmobile2
+            // 
+            this.pnmobile2.Location = new System.Drawing.Point(12, 321);
+            this.pnmobile2.Name = "pnmobile2";
+            this.pnmobile2.Size = new System.Drawing.Size(519, 22);
+            this.pnmobile2.TabIndex = 1;
+            // 
+            // pndirectline
+            // 
+            this.pndirectline.Location = new System.Drawing.Point(12, 349);
+            this.pndirectline.Name = "pndirectline";
+            this.pndirectline.Size = new System.Drawing.Size(519, 22);
+            this.pndirectline.TabIndex = 1;
+            // 
+            // pnfax
+            // 
+            this.pnfax.Location = new System.Drawing.Point(12, 377);
+            this.pnfax.Name = "pnfax";
+            this.pnfax.Size = new System.Drawing.Size(519, 22);
+            this.pnfax.TabIndex = 1;
+            // 
+            // email1
+            // 
+            this.email1.Location = new System.Drawing.Point(12, 405);
+            this.email1.Name = "email1";
+            this.email1.Size = new System.Drawing.Size(519, 22);
+            this.email1.TabIndex = 1;
+            // 
+            // email2
+            // 
+            this.email2.Location = new System.Drawing.Point(12, 433);
+            this.email2.Name = "email2";
+            this.email2.Size = new System.Drawing.Size(519, 22);
+            this.email2.TabIndex = 1;
+            // 
+            // imskype
+            // 
+            this.imskype.Location = new System.Drawing.Point(12, 461);
+            this.imskype.Name = "imskype";
+            this.imskype.Size = new System.Drawing.Size(519, 22);
+            this.imskype.TabIndex = 1;
+            // 
+            // immsn
+            // 
+            this.immsn.Location = new System.Drawing.Point(12, 489);
+            this.immsn.Name = "immsn";
+            this.immsn.Size = new System.Drawing.Size(519, 22);
+            this.immsn.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 761);
+            this.Controls.Add(this.immsn);
+            this.Controls.Add(this.imskype);
+            this.Controls.Add(this.email2);
+            this.Controls.Add(this.email1);
+            this.Controls.Add(this.pnfax);
+            this.Controls.Add(this.pndirectline);
+            this.Controls.Add(this.pnmobile2);
+            this.Controls.Add(this.pnmobile1);
+            this.Controls.Add(this.pncompany);
+            this.Controls.Add(this.addrcompany2);
+            this.Controls.Add(this.addrcompany1);
+            this.Controls.Add(this.companywebsite);
+            this.Controls.Add(this.companyname);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.suffix);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.prefix);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox prefix;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox suffix;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.TextBox companyname;
+        private System.Windows.Forms.TextBox companywebsite;
+        private System.Windows.Forms.TextBox addrcompany1;
+        private System.Windows.Forms.TextBox addrcompany2;
+        private System.Windows.Forms.TextBox pncompany;
+        private System.Windows.Forms.TextBox pnmobile1;
+        private System.Windows.Forms.TextBox pnmobile2;
+        private System.Windows.Forms.TextBox pndirectline;
+        private System.Windows.Forms.TextBox pnfax;
+        private System.Windows.Forms.TextBox email1;
+        private System.Windows.Forms.TextBox email2;
+        private System.Windows.Forms.TextBox imskype;
+        private System.Windows.Forms.TextBox immsn;
+    }
+}
+
---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        [DataContract]
+        class Person
+        {
+            [DataMember]
+            internal string name;
+
+            [DataMember]
+            internal int age;
+        }
+
+        [DataContract]
+        class Addr
+        {
+            [DataMember]
+            internal string Company1;
+
+            [DataMember]
+            internal string Company2;
+        }
+
+        [DataContract]
+        class pn
+        {
+            [DataMember]
+            internal string Company;
+
+            [DataMember]
+            internal string Mobile1;
+
+            [DataMember]
+            internal string Mobile2;
+
+            [DataMember]
+            internal string Directline;
+
+            [DataMember]
+            internal string Fax;
+        }
+
+        [DataContract]
+        class email
+        {
+            [DataMember]
+            internal string Email1;
+            [DataMember]
+            internal string Email2;
+        }
+
+        [DataContract]
+        class im
+        {
+            [DataMember]
+            internal string skype;
+            [DataMember]
+            internal string MSN;
+        }
+
+
+
+        [DataContract]
+        class Card
+        {
+            [DataMember]
+            internal string Prefix;
+
+            [DataMember]
+            internal string Name;
+
+            [DataMember]
+            internal string Suffix;
+
+            [DataMember]
+            internal string Title;
+
+            [DataMember]
+            internal string CompanyName;
+
+            [DataMember]
+            internal string CompanyWebsite;
+
+            [DataMember]
+            internal Addr Addr;
+
+            [DataMember]
+            internal pn PhoneNumber;
+
+            [DataMember]
+            internal email Email;
+
+            [DataMember]
+            internal im IM;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.Text = "打开";
+            set1();
+        }
+
+
+        private void showcard(Card p3)
+        {
+            prefix.Text = p3.Prefix;
+            name.Text = p3.Name;
+            suffix.Text = p3.Suffix;
+            title.Text = p3.Title;
+            companyname.Text = p3.CompanyName;
+            companywebsite.Text = p3.CompanyWebsite;
+            addrcompany1.Text = p3.Addr.Company1;
+            addrcompany2.Text = p3.Addr.Company2;
+            pncompany.Text = p3.PhoneNumber.Company;
+            pnmobile1.Text = p3.PhoneNumber.Mobile1;
+            pnmobile2.Text = p3.PhoneNumber.Mobile2;
+            pndirectline.Text = p3.PhoneNumber.Directline;
+            pnfax.Text = p3.PhoneNumber.Fax;
+            email1.Text = p3.Email.Email1;
+            email2.Text = p3.Email.Email2;
+            imskype.Text = p3.IM.skype;
+            immsn.Text = p3.IM.MSN;
+        }
+
+        private void set1()
+        {
+            Card label = new Card();
+            label.Addr = new Addr();
+
+            label.Addr.Company1 = "Company1 address";
+            label.Addr.Company2 = "Company2 address";
+            label.CompanyName = "Company Name";
+            label.CompanyWebsite = "Company Website";
+            label.Email = new email();
+
+            label.Email.Email1 = "Email1";
+            label.Email.Email2 = "Email2";
+            label.IM = new im();
+
+            label.IM.MSN = "MSN";
+            label.IM.skype = "skype";
+            label.Name = "Name";
+            label.PhoneNumber = new pn();
+
+            label.PhoneNumber.Company = "Company Phone Number";
+            label.PhoneNumber.Directline = "Directline";
+            label.PhoneNumber.Fax = "Fax";
+            label.PhoneNumber.Mobile1 = "Mobile Phone Number 1";
+            label.PhoneNumber.Mobile2 = "Mobile Phone Number 2";
+
+            label.Prefix = "Prefix";
+            label.Suffix = "Suffix";
+            label.Title = "Tilte";
+
+            showcard(label);
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Person p = new Person();
+            //p.name = "John";
+            //p.age = 42;
+
+            //MemoryStream stream1 = new MemoryStream();
+
+            //Serialize the Person object to a memory stream using DataContractJsonSerializer.
+            //DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Person));
+            //ser.WriteObject(stream1, p);
+
+            //Show the JSON output.
+            //stream1.Position = 0;
+            //StreamReader sr = new StreamReader(stream1);
+
+            //Console.Write("JSON form of Person object: ");
+            //Console.WriteLine(sr.ReadToEnd());
+
+
+
+            //Deserialize the JSON back into a new Person object.
+            //stream1.Position = 0;
+            //Person p2 = (Person)ser.ReadObject(stream1);
+            //Person p2 = (Person)ser.ReadObject(sr.BaseStream);
+            //byte[] by = new byte(str);
+
+
+            //Show the results.
+            //Console.Write("Deserialized back, got name=");
+            //Console.Write(p2.name);
+            //Console.Write(", age=");
+            //Console.WriteLine(p2.age);
+
+
+            //Console.WriteLine("Press <ENTER> to terminate the program.");
+            //Console.ReadLine();
+            //////////////////////////////////////////////////////////////////////////////////////
+
+            //StreamReader sr = new StreamReader("j.txt",System.Text.Encoding.GetEncoding("gb2312"));
+            //String str;
+            //str = sr.ReadToEnd();
+            //MessageBox.Show(str);
+            //DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Person));
+            //MemoryStream stream2 = new MemoryStream(Encoding.UTF8.GetBytes(str));
+            //Person p2 = (Person)ser.ReadObject(stream2);
+
+            //MessageBox.Show("Deserialized back, got name=" + p2.name + ", 年龄=" + p2.age);
+
+
+            ///////////////////////////////////////////////////////////////
+
+
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            // Insert code to read the stream here.
+
+                            ///////////////////////////////////////////////////////////////////////////
+                            //DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Person));
+                            //Person p2 = (Person)ser.ReadObject(myStream);
+                            //MessageBox.Show("Deserialized back, got name=" + p2.name + ", age=" + p2.age1);
+                            //////////////////////////////////////////////////////////////////////////////
+
+
+
+                            //DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Card));
+                            //Card p4 = (Card)ser.ReadObject(myStream);
+                            //showcard(p4);
+                        }
+
+
+                        StreamReader sr = new StreamReader(openFileDialog1.FileName, Encoding.GetEncoding("gb2312"));
+                        //MessageBox.Show(sr.ReadToEnd());
+                        MemoryStream stream2 = new MemoryStream(Encoding.UTF8.GetBytes(sr.ReadToEnd()));
+                        DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Card));
+                        Card p4 = (Card)ser.ReadObject(stream2);
+                        showcard(p4);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+
+
+        }
+    }
+}
